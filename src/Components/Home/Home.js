741c604b88706f25b59a64b5a76f110a87366f32
Sadnan-2019 <sadnan.wow@gmail.com>
2023-06-22 16:46:09 +0600

    careeara

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,12 +1,23 @@
 import React from 'react';
 // import Landing from "./"
-import Landing from "../../Components/Landing/Landing"
+import Landing from "../../Components/Landing/Landing";
+import Service from "../../Components/Service/Service";
+import Review from "../../Components/Review/Review";
+// import Working from "../../Components/Working/Working";
+import Working from "../../Components/Working/Working";
+import Clients from "../../Components/Clients/Clients";
+import Contact from "../../Components/Contact/Contact";
 
 
 const Home = () => {
     return (
         <div>
     <Landing></Landing>
+    <Service></Service>
+    <Review></Review>
+    <Working></Working>
+    <Clients></Clients>
+    <Contact></Contact>
         </div>
     );
 };
