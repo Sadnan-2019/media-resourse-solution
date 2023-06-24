b676c27083487cfa19ca1a68d2737887627137aa
Sadnan-2019 <sadnan.wow@gmail.com>
2023-06-24 14:58:48 +0600

    news ana meida

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import AboutLanding from './AboutLanding';
+import Team from './Team';
 
 const About = () => {
     return (
         <div>
-            
+            <AboutLanding></AboutLanding>
+            <Team></Team>
         </div>
     );
 };
