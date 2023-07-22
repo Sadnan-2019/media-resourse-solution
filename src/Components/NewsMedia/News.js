ce8e079c049ece221b1bfecae8d5a78aba073ff4
Sadnan-2019 <sadnan.wow@gmail.com>
2023-07-22 17:38:39 +0600

    responsive

--- src/Components/NewsMedia/News.js
+++ src/Components/NewsMedia/News.js
@@ -5,6 +5,8 @@ import rose from "../../assets/mrglogo (7).png"
 import mrg from "../../assets/mrglogo (1).png"
 import tmcc from "../../assets/mrglogo (4).png"
 import tcc from "../../assets/mrglogo (3).png"
+import tgr from "../../assets/mrglogo (5).png"
+import innrah from "../../assets/inaarah.jpeg"
 
 // import rose from "../../assets/makeover.png"
 import conserns1 from "../../assets/healthcare.png"
@@ -108,6 +110,44 @@ const News = () => {
                 >
                   <img
                     src={rose}
+                    class="w-full"
+                    alt="Louvre"
+                  />
+                  <a href="#!">
+                    <div class="absolute top-0 right-0 bottom-0 left-0 h-full w-full overflow-hidden bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-100 bg-[hsla(0,0%,98.4%,.15)]"></div>
+                  </a>
+                </div>
+              </div>
+
+              <div class="mb-6 mr-auto w-full shrink-0 grow-0 basis-auto px-3 md:mb-0 md:w-9/12 xl:w-7/12">
+                <h5 class="mb-3 text-lg font-bold">Welcome to Rose Beauty Garden</h5>
+                <div class="mb-3 gap-2 flex items-center justify-center text-sm font-medium text-yellow-600 md:justify-start">
+                <img src={conserns3} alt="" className="w-6 text-black" />
+
+                  Makeover
+                </div>
+                <p class="mb-6 text-neutral-500 dark:text-neutral-300">
+                  <small>
+                    Published <u>10.01.2022</u> by
+                    <a href="#!">Joe Svan</a>
+                  </small>
+                </p>
+                <p class="text-neutral-500 dark:text-neutral-300">
+                  Curabitur tristique, mi a mollis sagittis, metus felis mattis
+                  arcu, non vehicula nisl dui quis diam. Mauris ut risus eget
+                  massa volutpat feugiat. Donec.
+                </p>
+              </div>
+            </div>
+            <div class="mb-6 flex flex-wrap">
+              <div class="mb-6 ml-auto w-full shrink-0 grow-0 basis-auto px-3 md:mb-0 md:w-3/12">
+                <div
+                  class="relative mb-6 overflow-hidden rounded-lg bg-cover bg-no-repeat shadow-lg dark:shadow-black/20"
+                  data-te-ripple-init
+                  data-te-ripple-color="light"
+                >
+                  <img
+                    src={tgr}
                     class="w-full"
                     alt="Louvre"
                   />
@@ -255,6 +295,56 @@ const News = () => {
                 >
                   <img
                     src={tcc}
+                    class="w-full"
+                    alt="Louvre"
+                  />
+                  <a href="#!">
+                    <div class="absolute top-0 right-0 bottom-0 left-0 h-full w-full overflow-hidden bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-100 bg-[hsla(0,0%,98.4%,.15)]"></div>
+                  </a>
+                </div>
+              </div>
+
+              <div class="mb-6 mr-auto w-full shrink-0 grow-0 basis-auto px-3 md:mb-0 md:w-9/12 xl:w-7/12">
+                <h5 class="mb-3 text-lg font-bold">Welcome to Trishal Convention Center</h5>
+                <div class="mb-3 flex items-center justify-center text-sm font-medium text-yellow-600 md:justify-start">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="2"
+                    stroke="currentColor"
+                    class="mr-2 h-5 w-5"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M2.25 18.75a60.07 60.07 0 0115.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 013 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 00-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 01-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 003 15h-.75M15 10.5a3 3 0 11-6 0 3 3 0 016 0zm3 0h.008v.008H18V10.5zm-12 0h.008v.008H6V10.5z"
+                    />
+                  </svg>
+                  Business
+                </div>
+                <p class="mb-6 text-neutral-500 dark:text-neutral-300">
+                  <small>
+                    Published <u>10.01.2022</u> by
+                    <a href="#!">Joe Svan</a>
+                  </small>
+                </p>
+                <p class="text-neutral-500 dark:text-neutral-300">
+                  Curabitur tristique, mi a mollis sagittis, metus felis mattis
+                  arcu, non vehicula nisl dui quis diam. Mauris ut risus eget
+                  massa volutpat feugiat. Donec.
+                </p>
+              </div>
+            </div>
+            <div class="mb-6 flex flex-wrap">
+              <div class="mb-6 ml-auto w-full shrink-0 grow-0 basis-auto px-3 md:mb-0 md:w-3/12">
+                <div
+                  class="relative mb-6 overflow-hidden rounded-lg bg-cover bg-no-repeat shadow-lg dark:shadow-black/20"
+                  data-te-ripple-init
+                  data-te-ripple-color="light"
+                >
+                  <img
+                    src={innrah}
                     class="w-full"
                     alt="Louvre"
                   />
