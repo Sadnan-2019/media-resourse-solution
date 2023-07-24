4db45350b377702a6357b8337708fcc87899694c
Sadnan-2019 <sadnan.wow@gmail.com>
2023-07-24 11:53:53 +0600

    AI

--- src/Components/Carrer/CarrerLanding.js
+++ src/Components/Carrer/CarrerLanding.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import "./CarrerLanding.css"
+import React from "react";
+import "./CarrerLanding.css";
 // import carrer from "../../assets/careearlanding.jpg"
 const CarrerLanding = () => {
-    return (
-        <div>
-            <section className="carrerlanding    blog         ">
+  return (
+    <div>
+      <section className="carrerlanding    blog         ">
         <div className=" md:py-20 py-10 lg:py-24 lg:mx-16 mx-8 grid grid-cols-1 lg:grid-cols-2  gap-5 justify-center items-center">
           <div className="sm:max-w-xs lg:max-w-xl text-left md:ml-8  order-2   lg:mt-0">
             <h2
@@ -12,17 +12,17 @@ const CarrerLanding = () => {
               className="md:text-[43px] font-bold text-[20px] text-left   text-transparent text-8xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 "
             >
               {" "}
-              Do you have what it takes to join MRS?
+              Artificial intelligence (AI) is the intelligence of machines or
+              software, as opposed to the intelligence of human beings or
+              animals.
             </h2>
 
-            <h1
+            {/* <h1
               className="mb-5 text-lg  lg:text-2xl max-w-lg mt-7 font-bold   md:text-[25px] poppins-t "
               style={{ lineHeight: "120%", color: "white" }}
             >
               Work Together For Success{" "}
-            </h1>
-
-             
+            </h1> */}
           </div>
 
           <div class="  sm:max-w-xs lg:max-w-sm text-left md:ml-8 lg:order-1 order-2   lg:mt-0  ">
@@ -67,10 +67,9 @@ const CarrerLanding = () => {
             class="transition-all duration-300 ease-in-out delay-150 path-0"
           ></path>
         </svg> */}
-       
       </section>
-        </div>
-    );
+    </div>
+  );
 };
 
 export default CarrerLanding;
