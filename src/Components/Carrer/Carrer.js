6993e95b5de02a55605ec2a6c940a338f7067e3b
Sadnan-2019 <sadnan.wow@gmail.com>
2023-07-25 16:36:26 +0600

    AI

--- src/Components/Carrer/Carrer.js
+++ src/Components/Carrer/Carrer.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import CarrerLanding from './CarrerLanding';
 import CarrerForm from './CarrerForm';
+import Mytimeline from '../Mytimeline/Mytimeline';
 
 const Carrer = () => {
     return (
         <div>
             <CarrerLanding></CarrerLanding>
             <CarrerForm></CarrerForm>
+            <Mytimeline></Mytimeline>
         </div>
     );
 };
