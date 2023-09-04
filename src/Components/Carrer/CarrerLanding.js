f76c6d87c792ac09a255aaa97ff39efdfc7b34f6
Sadnan-2019 <sadnan.wow@gmail.com>
2023-09-04 17:11:14 +0600

    contacts

--- src/Components/Carrer/CarrerLanding.js
+++ src/Components/Carrer/CarrerLanding.js
@@ -1,28 +1,67 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./CarrerLanding.css";
+import { AnimatePresence, motion } from "framer-motion";
 // import carrer from "../../assets/careearlanding.jpg"
 const CarrerLanding = () => {
+  const sentences = [
+    'Artificial intelligence (AI) is the intelligence of machines',
+    'or software, as opposed to the intelligence of human beings',
+    'or animals.',
+    // Add more sentences as needed
+  ];
+const [currentIndex, setCurrentIndex] = useState(0);
+
+useEffect(() => {
+  const interval = setInterval(() => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % sentences.length);
+  }, 2000); // Change the duration as needed
+
+  return () => clearInterval(interval);
+}, [sentences.length]);
+
+
+
+
+
+
   return (
     <div>
       <section className="carrerlanding    blog         ">
         <div className=" md:py-20 py-10 lg:py-24 lg:mx-16 mx-8 grid grid-cols-1 lg:grid-cols-2  gap-5 justify-center items-center">
-          <div className="sm:max-w-xs lg:max-w-xl text-left md:ml-8  order-2   lg:mt-0">
-            <h2
+          <div className=" sm:max-w-xs lg:max-w-xl text-left md:ml-8  order-2   lg:mt-0">
+            {/* <h2
               style={{ lineHeight: "120%" }}
-              className="md:text-[43px] font-bold text-[20px] text-left   text-transparent text-8xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 "
+              className=" md:text-[43px] font-bold text-[20px] text-left   text-transparent text-8xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 "
             >
               {" "}
               Artificial intelligence (AI) is the intelligence of machines or
               software, as opposed to the intelligence of human beings or
               animals.
-            </h2>
-
+            </h2> */}
+            
+            <div className="text-container">
+    <AnimatePresence mode='wait'>
+      <motion.h2
+        key={currentIndex}
+        initial={{ opacity: 0, y: 50 }}
+        animate={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 0, y: -50 }}
+        transition={{ duration: 1 }}
+        style={{ lineHeight: "120%" }}
+        className="animated-text md:text-[43px] font-bold text-[20px] text-left   text-transparent text-8xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600"
+      >
+        {sentences[currentIndex]}
+      </motion.h2>
+    </AnimatePresence>
+  </div>
             {/* <h1
               className="mb-5 text-lg  lg:text-2xl max-w-lg mt-7 font-bold   md:text-[25px] poppins-t "
               style={{ lineHeight: "120%", color: "white" }}
             >
               Work Together For Success{" "}
             </h1> */}
+
+         
           </div>
 
           <div class="  sm:max-w-xs lg:max-w-sm text-left md:ml-8 lg:order-1 order-2   lg:mt-0  ">
