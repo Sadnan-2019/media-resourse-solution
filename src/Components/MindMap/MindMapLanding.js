bcf2b60406639c4032569e98ccc0245135f03dc6
Sadnan-2019 <sadnan.wow@gmail.com>
2024-11-11 12:32:17 +0600

    video

--- src/Components/MindMap/MindMapLanding.js
+++ src/Components/MindMap/MindMapLanding.js
@@ -1,38 +1,36 @@
 import React from "react";
+import { BsArrowRight } from "react-icons/bs";
 
 const MindMapLanding = () => {
   return (
     <div>
-      <section class="pt-10 overflow-hidden bg-gray-50 dark:bg-gray-800 md:pt-0 sm:pt-16 2xl:pt-16">
+      <section class="pt-10 overflow-hidden    d:pt-0 sm:pt-16 2xl:pt-16">
         <div class="px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
           <div class="grid items-center grid-cols-1 md:grid-cols-2">
             <div>
               <h2 class="text-3xl font-bold leading-tight text-black dark:text-white sm:text-4xl lg:text-5xl">
-                Hey 👋 Me 
+                Hey 👋 Me
                 <br class="block sm:hidden" />
-                 From Mind Map
+                From Mind Map
               </h2>
               <p class="max-w-lg mt-3 text-xl leading-relaxed text-gray-600 dark:text-gray-300 md:mt-8">
                 Amet minim mollit non deserunt ullamco est sit aliqua dolor do
                 amet sint. Velit officia consequat duis enim velit mollit.
-                Exercitation veniam consequat sunt nostrud amet.
+                Exercitation veniam consequat sunt nostrud amet.BOOKED YOUR DOCTOR                                                                                          
               </p>
 
-              <p class="mt-4 text-xl text-gray-600 dark:text-gray-300 md:mt-8">
-                <span class="relative inline-block">
-                  <span class="absolute inline-block w-full bottom-0.5 h-2 bg-yellow-300 dark:bg-gray-900"></span>
-                  <span class="relative"> Have a question? </span>
-                </span>
-                <br class="block sm:hidden" />
-                Ask me on{" "}
+              <div className=" ">
                 <a
-                  href="#"
-                  title=""
-                  class="transition-all duration-200 text-sky-500 dark:text-sky-400 hover:text-sky-600 dark:hover:text-sky-500 hover:underline"
+                  href="/"
+                  className="  btn-sm lg:p-2 rounded-full      bg-[#38235D] inline-flex  md:inline-flex items-center     border text-white "
                 >
-                  Twitter
+                  <span className="text-sm font-medium lg:text-lg">
+                    {" "}
+                    More Info
+                  </span>
+                  <BsArrowRight className="text-lg hidden lg:ml-2" />
                 </a>
-              </p>
+              </div>
             </div>
 
             <div class="relative">
